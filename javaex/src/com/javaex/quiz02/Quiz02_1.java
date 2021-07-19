68785c288f808721427b67fa4d6c245c2690e9d9
박헌주 <fly1004a@naver.com>
2021-07-19 17:00:09 +0900

    첫번째 퀴즈

---
+++ javaex/src/com/javaex/quiz02/Quiz02_1.java
@@ -0,0 +1,30 @@
+package com.javaex.quiz02;
+
+public class Quiz02_1 {
+	public static void main(String[] args) {
+		int a = 3;
+		int b = 6;
+		char c = 'A';
+		
+		// a는 짝수인가?
+		System.out.println(a % 2 == 0);
+		// b는 홀수인가?
+		System.out.println(b % 3 == 0);
+		
+		boolean r1 = a % 2 == 0 && b % 3 == 0;
+		System.out.println(r1);
+		
+		System.out.println(c + 4);
+		
+		char r2 = (char)(c+4);
+		
+		System.out.println(r2);
+	}
+		
+		
+			
+		
+	    		
+		
+	
+}
